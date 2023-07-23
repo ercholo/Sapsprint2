a62f3f04832c3133bad9bf13b4314d680f90dcec
Gregorio PCCasa <ercholo1@yahoo.es>
2023-07-23 11:49:27 +0200

    he cambiado de tabla

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import CustomPaginationActionsTable from './components/tablas.jsx'
+import CustomPaginationActionsTable from './components/tablas1.jsx'
+import StickyHeadTable from './components/tablas.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <CustomPaginationActionsTable />
+    <App /> 
+    <StickyHeadTable />
   </React.StrictMode>,
 )
