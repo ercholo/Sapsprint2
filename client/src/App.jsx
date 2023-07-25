ae7d854f4db8ab4a673b5f087584cdded4e5efa2
Gregorio PC Hefame <ercholo1@yahoo.es>
2023-07-25 14:20:16 +0200

    el array llega, pero no el objeto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState([]);
 
-  React.useEffect(() => {
-    // fetch("http://172.30.5.181:4444/impresoras")
-    fetch("http://localhost:4444/impresoras")
-      .then((res) => res.json())
-      .then((data) => setData(data.numeroDeTrabajos))
-      .catch(error => console.log(error));
-    }, []);
-
+  useEffect(() => {
+    const getImpresoras = async () => {
+      const res = await fetch('http://172.30.5.181:4444/impresoras');
+      const data = await res.json();
+      console.log(data.numeroDeTrabajos)
+      setData(data.numeroDeTrabajos);
+      console.log(data)
+    };
+    getImpresoras();
+  }, []);
 
   return (
-    <p>Impresora local: {data}</p>
+    <div>
+      {
+        data ? <p>Impresora: {data}</p> : <p>Loading...</p>
+      }
+    </div>
   );
 }
 
--- routes/impresoras.js
+++ routes/impresoras.js
@@ -1,22 +1,25 @@
-const {Router} = require('express');
+const { Router, request } = require('express');
 const router = Router();
-const {response } = require('express');
+const { response } = require('express');
 const trabajos = require('../devuelveTrabajos');
 let numeroPeticiones = 0;
+const impresoras = ["16ALAV101", "16ALAV102", "16ALAV201", "16ALAV202", "16ALDEV01", "16ALETQ01", "16ALETQ02", "16ALETQ03", "16ALEXP01", "16ALJEF01", "17ADCOM01", "17ALAV101", "17ALAV102", "17ALDEV01", "17ALGVO01", "17ALJEF01", "17ATTOM01", "18ALAV101", "18ALAV102", "18ALAV201", "18ALAV202", "18ALETQ01", "18ALETQ02", "18ALETQ03", "18ALEXP01", "18ALJEF01"]
 
 router.get('/', async (req, res = response) => {
 
-    let promesa = trabajos('\\\\ctxprinter\\01SIPRO01');
+    let request = impresoras.map(impresora => trabajos(impresora))
 
-    promesa.then((valor) => {
-        res.json({ numeroDeTrabajos: parseInt(valor)});
-    })
-    .catch((error) => {
-        res.status(500).json({error: error.message})
-    })
-    .finally(() => {
-        numeroPeticiones++;
-    })
+    Promise.allSettled(request)
+        // .then((valor) => {
+        //     res.json({ numeroDeTrabajos: parseInt(valor)});
+        // })
+        .then((response) => res.json(response))
+        .catch((error) => {
+            res.status(500).json({ error: error.message })
+        })
+        .finally(() => {
+            numeroPeticiones++;
+        })
 });
 // -----------------------
 
@@ -28,7 +31,7 @@ router.get('/', async (req, res = response) => {
 //     } finally {
 //         numeroPeticiones++;
 //     }
-    
+
 //   });
 
 module.exports = router;
